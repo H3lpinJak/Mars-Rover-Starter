86a06bfcc00d9523449a5f9ff8152c7441b2b102
Ricky <ricky@Luiss-Air.lan>
2024-04-28 22:37:44 -0500

    command.js

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -3,7 +3,20 @@ const Command = require('../command.js');
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
-
+let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+let message = new Message('Test message with two commands', commands);
 describe("Message class", function() {
-
+    
+    it("throws error if command type is NOT passed into constructor as the first parameter", function () {
+        expect(function () { new Message(); }).toThrow(new Error('Message type required.'));
+    });
+    it("constructor sets name", function () {
+        expect(message.commands).toEqual(commands)
+    });
+    //Test 6
+    //The description reads “contains a commands array passed into the constructor as the 2nd argument”.
+    //confirm the command property of a new message object contains the data passed in from the Message(name, commands)
+    it("contains a commands array passed into the constructor as the 2nd argument", function () {
+        expect(commands.name).toBe("")
+    });
 });
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -5,9 +5,32 @@ const Command = require('../command.js');
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
+let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+let message = new Message('Test message with two commands', commands);
+let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+let response = rover.receiveMessage(message);
 
-describe("Rover class", function() {
+console.log(response);
+describe("Rover class", function () {
 
-  // 7 tests here!
+  it("constructor sets position and default values for mode and generatorWatts", function () {
+    let rover = new Rover(98382);
+    expect(rover.postion).toEqual(98382);
+    expect(rover.mode).toEqual("NORMAL");
+    expect(rover.generatorWatts).toEqual(110);
+});
+  it("response returned by receiveMessage contains the name of the message", function(){
+    let message = new Message('Test messsage', []);
+    let rover = new Rover(98382);
+    let response = rover.receiveMessage(message);
+    expect(response.message).toEqual('Test message');
+});
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+    let message = new Message('Test message with two commands', commands);
+    let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    let rover = new Rover(98382);
+    let response = rover.receiveMessage(message);
+    expect(response.message).toEqual();
+  });
 
 });
